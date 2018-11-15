b7664c9e49ebb8ddf5f5707dde4e0c2600f0595d
Flore Chasles <f.chasles@meetic-corp.com>
2018-11-15 13:46:46 +0100

    Add height zone1 + scroll sub nav

--- js/modules/surcharge.js
+++ js/modules/surcharge.js
@@ -1,10 +1,19 @@
-/**!
- * Slick init
-**/
-
 $(document).ready(function(){
+  /** Slick init **/
   $('.videos').slick({
     dots: true,
     arrows: true,
   });
+
+  /** Height zone1 + position **/
+  $('.article-intro').css('top', $('.at-main-title').position().top + $('.at-main-title').height() + 20 + 'px');
+
+  var headerHeight = $('.article-intro').height() + $('.at-main-title').height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav').height() + 60;
+
+  if (headerHeight > $('#zone1').height()) {
+    $('#zone1, .article-title img').css('height', headerHeight);
+  }
+
+  /** Scroll sub nav to active link **/
+  $('.ql-list.unstyled').animate({scrollLeft: parseInt($('.ql-item-link.is-active').position().left)}, 0);
 });
