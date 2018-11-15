3bf306ffb2cf6dd912fa6a4d47e64a67b0aaac53
Flore Chasles <f.chasles@meetic-corp.com>
2018-11-15 23:12:55 +0100

    Update js

--- js/modules/surcharge.js
+++ js/modules/surcharge.js
@@ -24,27 +24,31 @@ $(document).ready(function(){
   /** Height zone1 + position **/
   var $articleIntro = $('.article-intro');
   var $mainTitle = $('.at-main-title');
+  var $articleTitle = $('.article-title');
   if ($articleIntro.length > 0 && $mainTitle.length > 0) {
     $articleIntro.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
-    var headerHeight = $articleIntro.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav').height() + 60;
+    var headerHeight = $articleIntro.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
     if (headerHeight > $('#zone1').height()) {
       $('#zone1, .article-title img').css('height', headerHeight);
     }
-  }
-
-  var $articleTitle = $('.article-title');
-  if ($articleTitle.length > 0 && $mainTitle.length > 0) {
+  } else if ($articleTitle.length > 0 && $mainTitle.length > 0) {
     $articleTitle.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
-    var headerHeight = $articleTitle.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav').height() + 60;
+    var headerHeight = $articleTitle.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
     if (headerHeight > $('#zone1').height()) {
       $('#zone1, .article-title img').css('height', headerHeight);
     }
   }
 
   /** Scroll sub nav to active link **/
-  var $quickLinksList = $('.ql-list.unstyled');
+  var $quickLinksList = $('.quicklinks-navigation .ql-list.unstyled');
+  var quickLinksListWidth = 0;
+  $('.ql-item', $quickLinksList).each(function() {
+    var $self = $(this);
+    quickLinksListWidth += parseInt($self.width());
+  });
+
   if ($quickLinksList.length > 0) {
-    if ($quickLinksList.width() > $(document).width()) {
+    if (quickLinksListWidth > $(document).width()) {
       $quickLinksList.animate({scrollLeft: parseInt($('.ql-item-link.is-active').position().left)}, 0);
     } else {
       $quickLinksList.css('justify-content', 'center');
